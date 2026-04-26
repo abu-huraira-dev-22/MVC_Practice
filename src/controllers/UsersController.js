45802d801055051c6d4a7f34f5ed7f5aa523eaeb
Abu Huraira Ansari <ahurairaansari047@gmail.com>
2026-04-26 20:03:47 +0500

    update code

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -3,7 +3,11 @@ const Users = require("../models/UserMode");
 const getUsersController = async (req, res) => {
   let query = {};
   if (req.query.ageStart && req.query.ageEnd) {
-    query = {userName:req.query.userName, age: { $gte: req.query.ageStart, $lte: req.query.ageEnd } };
+    const queryFlag = {...req.query}
+    delete queryFlag.ageStart
+    delete queryFlag.ageEnd
+
+    query = {...queryFlag,age: { $gte: req.query.ageStart, $lte: req.query.ageEnd } };
   }
 
   const users = await Users.find(query);
