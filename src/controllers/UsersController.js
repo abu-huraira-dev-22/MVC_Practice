0795a31ba88e1a04def2dbbb73a2d48bd05775ce
Abu Huraira Ansari <your-github-email@example.com>
2026-04-30 23:21:13 +0500

    add functionality of update user api

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,17 +1,20 @@
 const Users = require("../models/UserMode");
-const jwt= require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
 const getUsersController = async (req, res) => {
   let query = {};
   if (req.query.ageStart && req.query.ageEnd) {
-    const queryFlag = {...req.query}
-    delete queryFlag.ageStart
-    delete queryFlag.ageEnd
+    const queryFlag = { ...req.query };
+    delete queryFlag.ageStart;
+    delete queryFlag.ageEnd;
 
-    query = {...queryFlag,age: { $gte: req.query.ageStart, $lte: req.query.ageEnd } };
+    query = {
+      ...queryFlag,
+      age: { $gte: req.query.ageStart, $lte: req.query.ageEnd },
+    };
   }
 
-  const users = await Users.find()
+  const users = await Users.find();
 
   res.json({
     status: true,
@@ -20,12 +23,26 @@ const getUsersController = async (req, res) => {
   });
 };
 
-const updateUserController = async(req,res)=>{
-  const updateDetails = req.body
-  const token = req.headers.authorization
-  console.log(token,"==>> token")
-  const decoded = jwt.verify(token,process.env.JWT_SECRET)
-  console.log(decoded)
-}
+const updateUserController = async (req, res) => {
+  try {
+    const updateDetails = req.body;
 
-module.exports = { getUsersController,updateUserController };
+    const token = req.headers.authorization.split(" ")[1];
+
+    console.log(token, "==>> token");
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    console.log(decoded);
+
+    await Users.findByIdAndUpdate(decoded.id, updateDetails);
+    res.json({
+      status: true,
+      message: "User updated Successfully",
+    });
+  } catch (error) {
+    console.log(error.message, "==>>error");
+  }
+};
+
+module.exports = { getUsersController, updateUserController };
