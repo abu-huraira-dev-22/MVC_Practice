54a0980e749739c13a79445c728e51cdb20273ba
Abu Huraira Ansari <your-github-email@example.com>
2026-05-01 22:51:57 +0500

    jwt expirey added

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,4 +1,4 @@
-const jwt= require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const Users = require("../models/UserMode");
 
 const signupController = async (req, res) => {
@@ -25,32 +25,41 @@ const signupController = async (req, res) => {
 };
 
 const loginController = async (req, res) => {
- try {
-   const { email, password } = req.body;
-  const myUser = await Users.findOne({
-    email: email,
-  });
+  try {
+    const { email, password } = req.body;
+    const myUser = await Users.findOne({
+      email: email,
+    });
 
-  if (!myUser) return res.json({
-    status:false,
-    message:"No User Found"
-  });
+    if (!myUser)
+      return res.json({
+        status: false,
+        message: "No User Found",
+      });
 
-  if (myUser.password != password) return res.json({
-    status:false,
-    message:"Invalid Credentials"
-  });
+    if (myUser.password != password)
+      return res.json({
+        status: false,
+        message: "Invalid Credentials",
+      });
 
-  const token = jwt.sign({email:myUser.email,userName:myUser.userName,id:myUser._id}, process.env.JWT_SECRET)
+    const token = jwt.sign(
+      {
+        email: myUser.email,
+        userName: myUser.userName,
+        id: myUser._id,
+      },
+      process.env.JWT_SECRET,{expiresIn: 2 * 60}
+    );
 
-  res.json({
-    status: true,
-    message: "User logged in Successfully",
-    token:token
-  });
- } catch (error) {
-  console.log(error.message, "==>>> error")
- }
+    res.json({
+      status: true,
+      message: "User logged in Successfully",
+      token: token,
+    });
+  } catch (error) {
+    console.log(error.message, "==>>> error");
+  }
 };
 
 module.exports = { signupController, loginController };
