bc351bd7cd6e55da99155c1f2ea4d2fd166b2300
Abu Huraira Ansari <your-github-email@example.com>
2026-05-01 19:56:05 +0500

    updated the file

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -6,10 +6,4 @@ const authRoutes = express.Router()
 authRoutes.post('/signup',signupController)
 authRoutes.post('/login',loginController)
 
-<<<<<<< HEAD
-// update code
-
-
-=======
->>>>>>> 0795a31ba88e1a04def2dbbb73a2d48bd05775ce
 module.exports = authRoutes
